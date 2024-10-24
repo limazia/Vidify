079997782dc558926bfa332b52e4de78cd03d66c
Acacio de Lima <limadeacacio@gmail.com>
2024-10-24 16:07:18 -0300

    refactor: Remove unused components and files

--- frontend/src/components/pulsing-gradient-circle.tsx
+++
@@ -1,7 +0,0 @@
-export function PulsingCircle() {
-  return (
-    <div className="flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-tr from-indigo-500 to-pink-500 animate-pulse">
-      <div className="h-9 w-9 rounded-full bg-white" />
-    </div>
-  );
-}
